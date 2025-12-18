b625097199bd7a2f3b8bd030442296eb386441f8
minsuRob <minsu.rob@gmail.com>
2025-12-18 13:11:27 +0900

    fix: 안드로이드 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,9 +2,14 @@ import makiBg from '../assets/maki.jpg'
 import '../App.css'
 
 function Home() {
+  const isAndroid = /Android/i.test(navigator.userAgent)
+  const storeUrl = isAndroid
+    ? 'https://play.google.com/store/apps/details?id=ink.markhub.app'
+    : 'https://apps.apple.com/kr/app/maki-by-markhub/id6746326048'
+
   return (
     <div className="app" style={{ backgroundImage: `url(${makiBg})` }}>
-      <a href="https://apps.apple.com/kr/app/maki-by-markhub/id6746326048" className="center-btn">Install MAKi</a>
+      <a href={storeUrl} className="center-btn">Install MAKi</a>
     </div>
   )
 }
