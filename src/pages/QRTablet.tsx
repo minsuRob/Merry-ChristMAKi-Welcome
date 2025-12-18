834e991978a91310a1b7812a04657ceb926fa30a
minsuRob <minsu.rob@gmail.com>
2025-12-18 11:57:05 +0900

    fix: 스캔 추가

--- src/pages/QRTablet.tsx
+++ src/pages/QRTablet.tsx
@@ -1,9 +1,11 @@
 import tabletBg from '../assets/tablet.jpg'
+import qrCode from '../assets/qr.png'
 import '../App.css'
 
 function QRTablet() {
   return (
-    <div className="app" style={{ backgroundImage: `url(${tabletBg})` }}>
+    <div className="app" style={{ backgroundImage: `url(${tabletBg})`, alignItems: 'center', paddingBottom: 0 }}>
+      <img src={qrCode} alt="QR Code" style={{ maxWidth: '300px', maxHeight: '300px' }} />
     </div>
   )
 }
